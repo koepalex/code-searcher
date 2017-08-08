8bc7d7aa45b34da25d631fa51a13415a23b0d208
koepalex <koepalex@xdigital.de>
2017-08-08 17:31:16 +0200

    update copyright information within AssemblyInfos

--- CodeSearcher.Gui/Properties/AssemblyInfo.cs
+++ CodeSearcher.Gui/Properties/AssemblyInfo.cs
@@ -9,7 +9,7 @@ using System.Runtime.CompilerServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("")]
-[assembly: AssemblyCopyright("Alex")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 

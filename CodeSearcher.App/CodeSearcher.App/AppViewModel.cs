d7a2cd7ac67ad4eeb1d48ec875870bc52c51f15a
Alexander Koepke <alexander.koepke@microsoft.com>
2020-10-09 17:10:13 +0200

    Moved controller functionality into view model

--- CodeSearcher.App/CodeSearcher.App/AppViewModel.cs
+++ CodeSearcher.App/CodeSearcher.App/AppViewModel.cs
@@ -2,13 +2,23 @@
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.ComponentModel;
+using System.Linq;
+using System.Net.Http;
 using System.Text;
+using System.Threading.Tasks;
+using CodeSearcher.BusinessLogic;
 using CodeSearcher.Interfaces;
+using CodeSearcher.Interfaces.API.Model.Requests;
+using CodeSearcher.Interfaces.API.Model.Response;
+using CodeSearcher.Interfaces.Constants;
+using Newtonsoft.Json;
 
 namespace CodeSearcher.App
 {
     public class AppViewModel : INotifyPropertyChanged
     {
+        private const string _baseAddress = @"http://localhost:5000";
+
         private string _statusMessage;
         public string StatusMessage
         {
@@ -23,6 +33,20 @@ namespace CodeSearcher.App
             }
         }
 
+        private ICodeSearcherIndex _selectedIndex;
+
+        public ICodeSearcherIndex SelectedIndex
+        {
+            get
+            {
+                return _selectedIndex;
+            }
+            set
+            {
+                _selectedIndex = value;
+                NotifyPropertChanged(nameof(SelectedIndex));
+            }
+        }
 
         public ObservableCollection<ICodeSearcherIndex> Indexes { get; set; }
 
@@ -37,6 +61,64 @@ namespace CodeSearcher.App
         public void NotifyPropertChanged(string propertyName)
         {
             PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        public async Task<IEnumerable<ICodeSearcherIndex>> LoadIndexesAsync()
+        {
+            using var client = new HttpClient();
+            client.BaseAddress = new Uri(_baseAddress);
+
+            var response = await client.GetAsync(APIRoutes.CodeSearcherRoute);
+
+            var responsePayload = await response.Content.ReadAsStringAsync();
+
+            var settings = new JsonSerializerSettings();
+            settings.Converters.Add(Factory.Get().GetCodeSearcherIndexJsonConverter());
+            var getIndexes = JsonConvert.DeserializeObject<GetIndexesResponse>(responsePayload, settings);
+
+            return getIndexes?.Indexes ?? Enumerable.Empty<ICodeSearcherIndex>();
+        }
+
+        public async Task<IEnumerable<IDetailedSearchResult>> LoadSearchResultAsync(string searchPattern)
+        {
+            if (SelectedIndex == null)
+            {
+                return Enumerable.Empty<IDetailedSearchResult>();
+            }
+
+            SearchIndexResponse searchResponse;
+            
+            using var client = new HttpClient();
+            client.BaseAddress = new Uri(_baseAddress);
+
+            var searchRequest = new SearchIndexRequest
+            {
+                IndexID = SelectedIndex.ID,
+                SearchWord = searchPattern
+            };
+
+            using (var requestPayload = new StringContent(JsonConvert.SerializeObject(searchRequest), Encoding.UTF8, "application/json"))
+            {
+                var request = new HttpRequestMessage
+                {
+                    Method = HttpMethod.Post,
+                    Content = requestPayload,
+                    RequestUri = new Uri(client.BaseAddress, APIRoutes.SearchInIndexRoute)
+                };
+                using (var response = await client.SendAsync(request))
+                {
+                    response.EnsureSuccessStatusCode();
+
+                    var responsePayload = await response.Content.ReadAsStringAsync();
+                    var settings = new JsonSerializerSettings();
+                    settings.Converters.Add(Factory.Get().GetDetailedResultJsonConverter());
+                    settings.Converters.Add(Factory.Get().GetFindingsInFileJsonConverter());
+                    searchResponse = JsonConvert.DeserializeObject<SearchIndexResponse>(responsePayload, settings);
+                }
+            }
+
+            return searchResponse?.Results ?? Enumerable.Empty<IDetailedSearchResult>();
+
         }
     }
 }
--- CodeSearcher.App/CodeSearcher.App/MainWindow.xaml.cs
+++ CodeSearcher.App/CodeSearcher.App/MainWindow.xaml.cs
@@ -31,7 +31,6 @@ namespace CodeSearcher.App
     /// </summary>
     public partial class MainWindow : Window
     {
-        private ICodeSearcherIndex _selectedIndex;
         private readonly AppViewModel _viewModel;
 
         public MainWindow()
@@ -59,62 +58,54 @@ namespace CodeSearcher.App
 
         private async void OnMainWindowLoaded(object sender, RoutedEventArgs e)
         {
-            using var client = new HttpClient();
-            client.BaseAddress = new Uri(@"http://localhost:5000");
-         
-            var response = await client.GetAsync("/api/CodeSearcher");
+            _viewModel.StatusMessage = "Loading existing indexes from server";
+            var indexes = await _viewModel.LoadIndexesAsync();
 
-            var responsePayload = await response.Content.ReadAsStringAsync();
-
-            var settings = new JsonSerializerSettings();
-            settings.Converters.Add(Factory.Get().GetCodeSearcherIndexJsonConverter());
-            var getIndexes = JsonConvert.DeserializeObject<GetIndexesResponse>(responsePayload, settings);
-
-            if (getIndexes != null)
+            foreach (var index in indexes)
             {
-                foreach (var index in getIndexes.Indexes)
+                var item = new TreeViewItem
                 {
-                    var item = new TreeViewItem
-                    {
-                        Header = index.SourcePath,
-                        Tag = index,
-                        Name = $"Item_{Math.Abs(index.ID).ToString()}",
-                        IsEnabled = true
-                    };
-                    IndexTreeView.Items.Add(item);
-                }
-
-                if (IndexTreeView.Items.Count > 0)
-                {
-                    ((TreeViewItem) IndexTreeView.Items[0]).IsSelected = true;
-                }
-                else
-                {
-                    _viewModel.StatusMessage = "Not Indexes available";
-                }
-
-                SearchTextBox.Focus();
+                    Header = index.SourcePath,
+                    Tag = index,
+                    Name = $"Item_{Math.Abs(index.ID).ToString()}",
+                    IsEnabled = true
+                };
+                IndexTreeView.Items.Add(item);
             }
+
+            if (IndexTreeView.Items.Count > 0)
+            {
+                ((TreeViewItem) IndexTreeView.Items[0]).IsSelected = true;
+                _viewModel.StatusMessage = "Loading Indexes finished";
+            }
+            else
+            {
+                _viewModel.StatusMessage = "Not Indexes available";
+            }
+
+            SearchTextBox.Focus();
         }
 
         private void IndexTreeViewSelectedItemChanged(object sender, RoutedPropertyChangedEventArgs<object> e)
         {
-            _selectedIndex = (IndexTreeView.SelectedItem as TreeViewItem)?.Tag as ICodeSearcherIndex;
+            _viewModel.SelectedIndex = (IndexTreeView.SelectedItem as TreeViewItem)?.Tag as ICodeSearcherIndex;
 
-            DeleteIndexButton.IsEnabled = _selectedIndex != null;
+            DeleteIndexButton.IsEnabled = _viewModel.SelectedIndex != null;
         }
-
 
         private void FindingTreeViewSelectionItemChanged(object sender, RoutedPropertyChangedEventArgs<object> e)
         {
             var file = ((FindingTreeView.SelectedItem as TreeViewItem)?.Parent as TreeViewItem)?.Tag as IDetailedSearchResult;
             var finding = (FindingTreeView.SelectedItem as TreeViewItem)?.Tag as IFindingInFile;
-            if (file != null && finding != null)
+            if (file != null && finding != null && File.Exists(file.Filename))
             {
-
                 TextEditor.Document = new TextDocument(new StringTextSource(File.ReadAllText(file.Filename)));
                 double vertOffset = (TextEditor.TextArea.TextView.DefaultLineHeight) * finding.LineNumber;
                 TextEditor.ScrollToVerticalOffset(vertOffset);
+            }
+            else
+            {
+                _viewModel.StatusMessage = $"Can't open file: {file?.Filename}, recreating the index should solve this";
             }
         }
 
@@ -135,69 +126,31 @@ namespace CodeSearcher.App
                 var searchPattern = (sender as TextBox)?.Text;
                 if (!string.IsNullOrWhiteSpace(searchPattern))
                 {
-                    SearchIndexResponse searchResponse;
-                    // Call search
-                    using var client = new HttpClient();
-                    client.BaseAddress = new Uri(@"http://localhost:5000");
 
-                    if (_selectedIndex == null)
+                    var results = await _viewModel.LoadSearchResultAsync(searchPattern);
+                    foreach (var findingResults in results)
                     {
-                        _viewModel.StatusMessage = "Please select index to search within";
-                        return;
-                    }
-
-                    var searchRequest = new SearchIndexRequest
-                    {
-                        IndexID = _selectedIndex.ID,
-                        SearchWord = searchPattern
-                    };
-
-                    using (var requestPayload = new StringContent(JsonConvert.SerializeObject(searchRequest), Encoding.UTF8, "application/json"))
-                    {
-                        var request = new HttpRequestMessage
+                        var item = new TreeViewItem
                         {
-                            Method = HttpMethod.Post,
-                            Content = requestPayload,
-                            RequestUri = new Uri(client.BaseAddress, APIRoutes.SearchInIndexRoute)
+                            Header = Path.GetFileName(findingResults.Filename),
+                            Tag = findingResults,
+                            Name = Path.GetFileNameWithoutExtension(findingResults.Filename),
+                            IsEnabled = true,
                         };
-                        using (var response = await client.SendAsync(request))
-                        {
-                            response.EnsureSuccessStatusCode();
 
-                            var responsePayload = await response.Content.ReadAsStringAsync();
-                            var settings = new JsonSerializerSettings();
-                            settings.Converters.Add(Factory.Get().GetDetailedResultJsonConverter());
-                            settings.Converters.Add(Factory.Get().GetFindingsInFileJsonConverter());
-                            searchResponse = JsonConvert.DeserializeObject<SearchIndexResponse>(responsePayload, settings);
-                        }
-                    }
-
-                    if (searchResponse != null)
-                    {
-                        foreach (var findingResults in searchResponse.Results)
+                        foreach (var lineFinding in findingResults.Findings)
                         {
-                            var item = new TreeViewItem
+                            var subItem = new TreeViewItem
                             {
-                                Header = Path.GetFileName(findingResults.Filename),
-                                Tag = findingResults,
-                                Name = Path.GetFileNameWithoutExtension(findingResults.Filename),
+                                Header = $"LineNumber: {lineFinding.LineNumber} Position: {lineFinding.Position} Length: {lineFinding.Length}",
+                                Tag = lineFinding,
+                                Name = $"Line{lineFinding.LineNumber}Pos{lineFinding.Position}Len{lineFinding.Length}",
                                 IsEnabled = true,
                             };
-
-                            foreach (var lineFinding in findingResults.Findings)
-                            {
-                                var subItem = new TreeViewItem
-                                {
-                                    Header = $"LineNumber: {lineFinding.LineNumber} Position: {lineFinding.Position} Length: {lineFinding.Length}",
-                                    Tag = lineFinding,
-                                    Name = $"Line{lineFinding.LineNumber}Pos{lineFinding.Position}Len{lineFinding.Length}",
-                                    IsEnabled = true,
-                                };
-                                item.Items.Add(subItem);
-                            }
-
-                            FindingTreeView.Items.Add(item);
+                            item.Items.Add(subItem);
                         }
+
+                        FindingTreeView.Items.Add(item);
                     }
                 }
             }

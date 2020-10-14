485c9366aea186a704f2e82735ecb80e93020cfb
Alexander Köpke <koepalex@xdigital.de>
2020-10-15 00:03:59 +0200

    code clean up

--- CodeSearcher.App/CodeSearcher.App/MainWindow.xaml.cs
+++ CodeSearcher.App/CodeSearcher.App/MainWindow.xaml.cs
@@ -1,29 +1,13 @@
-﻿using CodeSearcher.BusinessLogic;
-using CodeSearcher.Interfaces;
-using CodeSearcher.Interfaces.API.Model.Response;
-using Newtonsoft.Json;
+﻿using CodeSearcher.Interfaces;
 using System;
-using System.Collections.Generic;
 using System.ComponentModel;
-using System.Diagnostics;
 using System.IO;
 using System.Linq;
-using System.Net;
-using System.Net.Http;
-using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
-using CodeSearcher.Interfaces.API.Model.Requests;
-using CodeSearcher.Interfaces.Constants;
 using ICSharpCode.AvalonEdit.Document;
 using Path = System.IO.Path;
 
@@ -122,7 +106,7 @@ namespace CodeSearcher.App
             return sb.ToString();
         }
 
-        private async void OnMainWindowClosing(object sender, CancelEventArgs e)
+        private void OnMainWindowClosing(object sender, CancelEventArgs e)
         {
             DiagConsole.ProcessInterface.StopProcess();
         }

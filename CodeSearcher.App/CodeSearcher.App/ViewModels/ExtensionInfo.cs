485b67e6244189cfaa05fcd897dd006242fbe4ca
Alexander Köpke <koepalex@xdigital.de>
2021-11-04 00:23:29 +0100

    add Filter by file extension

---
+++ CodeSearcher.App/CodeSearcher.App/ViewModels/ExtensionInfo.cs
@@ -0,0 +1,37 @@
+﻿namespace CodeSearcher.App.ViewModels
+{
+    using System.ComponentModel;
+    using System.Runtime.CompilerServices;
+
+    public class ExtensionInfo : INotifyPropertyChanged
+    {
+        private string _extension;
+        public string Extension
+        {
+            get => _extension;
+            set
+            {
+                _extension = value;
+                NotifyPropertyChanged();
+            }
+        }
+
+        private bool _show;
+        public bool Show
+        {
+            get => _show;
+            set
+            {
+                _show = value;
+                NotifyPropertyChanged();
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void NotifyPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
